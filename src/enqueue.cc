a5cc0ba664a6f266f4b92de52cce4167651cb1af
Wei Zhang <wzam@amazon.com>
2021-10-27 00:13:53 +0000

    adjust nccl step from rashika@amazon.com
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- src/enqueue.cc
+++ src/enqueue.cc
@@ -298,7 +298,7 @@ static ncclResult_t getAlgoInfo(struct ncclInfo* info) {
     WARN("Error : no algorithm/protocol available");
     return ncclInternalError;
   }
-  //if (comm->rank == 0) INFO(NCCL_TUNING, "%ld Bytes -> Algo %d proto %d time %f", info->nBytes, info->algorithm, info->protocol, minTime);
+  if (comm->rank == 0) INFO(NCCL_TUNING, "%ld Bytes -> Algo %d proto %d time %f", info->nBytes, info->algorithm, info->protocol, minTime);
   TRACE(NCCL_COLL, "%ld Bytes -> Algo %d proto %d time %f", info->nBytes, info->algorithm, info->protocol, minTime);
 
   int nc = (info->nChannels > 0) ? info->nChannels :
